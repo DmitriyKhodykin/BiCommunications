211339e775967f9aed0fbdbd8f2aab2617841f17
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-12 08:45:59 +0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,10 @@
+# Ресурс и данные для авторизации ВАТС МТС
+token = '****'
+url_abonents = 'https://vpbx.mts.ru/api/abonents'
+url_callHistory = 'https://vpbx.mts.ru/api/callHistory'
+
+# Ресурс для авторизации в локальной базе
+host_vats = "****"
+user_vats = "****"
+passwd_vats = "****"
+db_vats = "vats"
