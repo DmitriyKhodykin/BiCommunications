34d410b86f9c4ced7d7fcde1fba7dc6ab8b9e2da
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-17 18:48:00 +0300

    Update auth.py
    RU to EN

--- auth.py
+++ auth.py
@@ -1,15 +1,15 @@
-# Ресурс и данные для авторизации ВАТС МТС
+# Resource for authorization in VATE
 token = '****'
 url_abonents = 'https://vpbx.mts.ru/api/abonents'
 url_callHistory = 'https://vpbx.mts.ru/api/callHistory'
 
-# Ресурс для авторизации в локальной базе
+# Resource for authorization in local database
 host_vats = "****"
 user_vats = "****"
 passwd_vats = "****"
 db_vats = "vats"
 
-# Ресурс для авторизации в SOAP / web-сервисе
+# Resource for authorization in SOAP-service
 wsdl = 'http://servicesite.ru/service.1cws?wsdl'
 user_soap = "****"
 password_soap = "****"
