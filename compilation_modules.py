76e97a6d4b53c0f1c4a1a8d386608316868aa2e6
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-17 18:51:54 +0300

    Update compilation_modules.py
    RU to EN

--- compilation_modules.py
+++ compilation_modules.py
@@ -1,11 +1,11 @@
-# Pyinstaller собирает python-приложение и все зависимости в один пакет.
-# В случае, если вы захотите собрать код в бинарном формате и запустить
-# его на сервере, то помочь в этом может Pyinstaller.
+# Pyinstaller builds a python application and all the dependencies in one package
+# In case you want to collect the code in binary format and run it on the server, 
+# then Pyinstaller can help
 
 # pip install pypiwin32
 # pip install pyinstaller
 
 pyinstaller --onefile --icon=name.ico --noconsole myscript.py
 
-# name.ico - иконка бинарного дистрибутива (в папке проекта)
-# --nonconsole - запуск программы в фоновом режиме
+# name.ico - binary distribution icon (in the project folder)
+# --nonconsole - running the program in the background
